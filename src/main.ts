3b906e9c4142ec21d6ef44d3f2977fd1f7eed5ff
unknown <C:\MyPrograms\TheBatPortable\Data\Mail>
2020-08-19 20:07:18 +0300

    app port config added

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
+import * as config from "config";
+
+const appConfig = config.get('app');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(process.env.PORT || appConfig.port);
 }
 bootstrap();
