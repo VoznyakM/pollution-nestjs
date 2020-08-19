301ade9be96bd45710d2d61bcd0bf63a8695378a
unknown <C:\MyPrograms\TheBatPortable\Data\Mail>
2020-08-19 16:58:40 +0300

    beanstalk preparation (config setup)

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,12 +1,15 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as config from "config";
+
+const dbConfig = config.get('db');
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
-  type: 'mysql',
-  host: 'localhost',
-  username: 'root',
-  password: '',
-  database: 'covid',
-  port: 3308,
+  type: dbConfig.type,
+  host: process.env.RDS_HOSTNAME || dbConfig.host,
+  username: process.env.RDS_USERNAME || dbConfig.username,
+  password: process.env.RDS_PASSWORD || dbConfig.password,
+  database: process.env.RDS_DB_NAME || dbConfig.database,
+  port: process.env.RDS_PORT || dbConfig.port,
   entities: [
     __dirname + '/../**/**.entity.{js,ts}',
   ],
